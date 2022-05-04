79d4ebdd7a98c37bd7faf664e7f184d639c1dc2d
silvester <silvester@ho.pt-saa.com>
2022-05-04 22:26:22 +0800

    update readme.md

--- README.md
+++ README.md
@@ -5,3 +5,32 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+## How to use
+
+Open your terminal
+
+clone from github repo
+
+```bash
+git clone https://github.com/silvesterwali/pre_test_front_end
+
+```
+
+go to pre_test_font_end directory
+
+```bash
+cd pre_test_front_end_directory
+```
+
+install all packages
+
+```bash
+ yarn install
+```
+
+run the server
+
+```bash
+yarn dev
+```
--- src/App.vue
+++ src/App.vue
@@ -161,29 +161,12 @@ watch(modal, (value) => {
           <a class="px-3 py-2 rounded-lg" href="/"> Home </a>
         </li>
 
-        <li><a class="px-3 py-2 rounded-lg" href=""> Projects </a></li>
-
         <li>
           <a
-            class="inline-flex items-center px-3 py-2 rounded-lg"
-            href=""
-            target="_blank"
+            class="px-3 py-2 rounded-lg"
+            href="https://github.com/silvesterwali/pre_test_front_end"
           >
-            External
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              class="ml-1.5 w-4 h-4"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              ></path>
-            </svg>
+            Silvester Wali
           </a>
         </li>
       </ul>
